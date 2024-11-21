38de7dc5af76db4f1bc1490dd8a1f6cdbad15619
MajoPino <majo12pino@gmail.com>
2024-11-21 09:18:23 -0500

    feat: Adding a new function to the EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -22,4 +22,19 @@ class EventController extends Controller
         // Pasamos las categorías a la vista 'categories.index'
         return view('events.adminIndex', compact('events'));
     }
+
+    public function destroy(string $id)
+    {
+        // Verifica si el usuario está autenticado y es administrador
+        /* if (!Auth::check() || Auth::user()->role_id != 1) { // 1 corresponde al rol de 'admin'
+            return redirect()->route('categories.index')->with('error', 'No tienes permiso para acceder a esta sección.');
+        } */
+        // Buscamos la categoría por ID y la eliminamos
+        $event = Event::findOrFail($id);
+        $event->delete();
+
+
+        // Redirigimos a la lista de categorías con un mensaje de éxito
+        return redirect()->route('events.adminIndex')->with('success', 'Categoría eliminada con éxito.');
+    }
 }
